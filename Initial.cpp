83d9302b19edb76550548e8db9c677f352a3522b
an <atianaim40@gmail.com>
2024-05-06 21:28:52 +0530

    Algo for square matices' product

--- Initial.cpp
+++ Initial.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int main()
 {
-    int n=3,s=0,i=0,j=0;
-    int arr3[i][j]={{0}};
-
-    int arr1[3][3]={{1,2,3},{1,2,3},{1,2,3}};
-    int arr2[3][3]={{1,2,3},{1,2,3},{1,2,3}};
-    cout<<"Algo for square matices' sum \n";
+    int n=3,s=0,i=0,j=0,k=0;
+    int arr3[3][3]={{0}};
+    int arr1[3][3]={{1,1,1},{2,2,2},{3,3,3}};
+    int arr2[3][3]={{1,1,1},{2,2,2},{3,3,3}};
+    cout<<"Algo for square matices' product \n";
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
-            arr3[i][j] = arr1[i][j]+arr2[i][j];
+            for (int k = 0; k < n; k++)
+            {
+               arr3[i][j] = arr3[i][j]+arr1[i][k]*arr2[k][j]; 
+            }
+            
         }
     }
     for(int i=0;i<n;i++)
