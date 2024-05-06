b1b714eae0683b7cb2ac95f3c2e5112ba75524b4
an <atianaim40@gmail.com>
2024-05-06 21:21:08 +0530

    Algo for square matices' sum

--- Initial.cpp
+++ Initial.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int n=5,s=0;
-    int arr[]={1,2,3,4,5};
-    cout<<"Algo for frequency count\n";
+    int n=3,s=0,i=0,j=0;
+    int arr3[i][j]={{0}};
+
+    int arr1[3][3]={{1,2,3},{1,2,3},{1,2,3}};
+    int arr2[3][3]={{1,2,3},{1,2,3},{1,2,3}};
+    cout<<"Algo for square matices' sum \n";
     for(int i=0;i<n;i++)
     {
-        s=s+arr[i];
+        for(int j=0;j<n;j++)
+        {
+            arr3[i][j] = arr1[i][j]+arr2[i][j];
+        }
     }
-    cout<<s;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr3[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
