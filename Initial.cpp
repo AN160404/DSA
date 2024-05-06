76db8a2bb5fa33ebc63115e0b5a6f866b4184e45
an <atianaim40@gmail.com>
2024-05-06 22:10:50 +0530

    Prime No

--- Initial.cpp
+++ Initial.cpp
@@ -3,30 +3,22 @@ using namespace std;
 
 int main()
 {
-    int n=3,s=0,i=0,j=0,k=0;
-    int arr3[3][3]={{0}};
-    int arr1[3][3]={{1,1,1},{2,2,2},{3,3,3}};
-    int arr2[3][3]={{1,1,1},{2,2,2},{3,3,3}};
-    cout<<"Algo for square matices' product \n";
-    for(int i=0;i<n;i++)
+    cout<<"Algo for prime no \n";
+    int n=1,count=0;
+    for (int i = 1; i<=n; i++)
     {
-        for(int j=0;j<n;j++)
+        if(n%i==0)
         {
-            for (int k = 0; k < n; k++)
-            {
-               arr3[i][j] = arr3[i][j]+arr1[i][k]*arr2[k][j]; 
-            }
-            
+            count=count+1;
         }
     }
-    for(int i=0;i<n;i++)
+    if (count<=2 && count>0)
     {
-        for(int j=0;j<n;j++)
-        {
-            cout<<arr3[i][j]<<" ";
-        }
-        cout<<endl;
+        cout<<"Prime";
     }
-    
+    else
+    {
+        cout<<"Not Prime";
+    }
     return 0;
 }
