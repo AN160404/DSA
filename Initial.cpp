518ee6c6f3c52b56735af541b2982d64899bf54e
an <atianaim40@gmail.com>
2024-05-12 15:00:07 +0530

    * ** ***

--- Initial.cpp
+++ Initial.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout<<"Pattern like:\n 1 2 3\n 4 5 6\n7 8 9";
-    int count=1,n=3;
+    cout<<"Pattern like:\n *\n **\n ***";
+    int n=3;
     cout<<"\n Output \n";
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j<=i; j++)
         {
-            cout<<count;
-            count++;
+            cout<<"*";
+            
         }
         cout<<endl;
     }
