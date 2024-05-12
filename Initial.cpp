5356f52a86f827387376cb27cac4ff35c0c9d9ab
an <atianaim40@gmail.com>
2024-05-12 15:11:17 +0530

    1 22 333

--- Initial.cpp
+++ Initial.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout<<"Pattern like:\n *\n **\n *** (while loop)";
+    cout<<"Pattern like:\n 1\n 22\n 333 (while loop)";
     int n=3,count=1;
     cout<<"\n Output \n";
     while (count <=n)
@@ -11,11 +11,22 @@ int main()
         int a=1;
         while (a<=count)
         {
-            cout<<"*";
+            cout<<count;
             a++;
         }
         cout<<"\n";
         count++;
+    }   
 
-    }    
+    cout<<"\n For Loop \n";
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1;j<=i; j++)
+        {
+            cout<<i;
+        }
+        cout<<"\n";
+    }
+        
 }
+    
