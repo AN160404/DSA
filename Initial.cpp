9bfed2c28033ddb522b6e5f20393d49891f1e568
an <atianaim40@gmail.com>
2024-05-14 14:23:27 +0530

    \n*** \n**\n*

--- Initial.cpp
+++ Initial.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main()
 {
-    cout<<"Pattern like:\n   *\n  **\n ***(while loop)";
+    cout<<"Pattern like:\n*** \n**\n*(while loop)";
     int n=3,row=1;
     cout<<"\n Output \n";
     while (row <=n)
     {
-        int space=n-row;
+        int space=n-row+1;
         while (space)
         {
-            cout<<" ";
+            cout<<"*";
             space--;
         }
         int col=1;
         while (col<=row)
         {
-            cout<<"*";
+            cout<<" ";
             col++;
         }
     row++;
