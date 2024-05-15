574d0385504eb76895c99c2a6f9d6d776f934593
an <atianaim40@gmail.com>
2024-05-15 15:49:06 +0530

    1234| 123|  12|   1

--- Initial.cpp
+++ Initial.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main() {
-    cout << "Pattern like:\n   1\n 22\n333(while loop)";
-    int n = 3, row = 1;
-    cout << "\n Output \n";
+    cout << "Pattern like:\n1234\n 123\n  12\n   1 (while loop)";
+    int n = 4, row = 1;
+    cout << "\nOutput\n";
     while (row <= n) {
-        int space=1;  // Start with 0 space in each row
-        while (space<=n-row) {  // Loop for spaces
+        int space = 1;  // Start with 1 space in each row
+        while (space <= row - 1) { // Loop for spaces
             cout << " ";
             space++;
         }
-        int col=1;
-        while (col<=row)
-        {
-            cout<<row;
+        int col = 1;
+        while (col<=n-row+1) {
+            cout << col;
             col++;
         }
         row++;
@@ -22,3 +21,4 @@ int main() {
     }
     return 0;
 }
+
