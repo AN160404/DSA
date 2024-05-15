d132bc89302394a1dc4e36468b33fdbfb1ec228c
an <atianaim40@gmail.com>
2024-05-15 16:16:06 +0530

    Pattern like: 1234554321 1234**4321 123****321 12******21 1********1

--- Initial.cpp
+++ Initial.cpp
@@ -2,28 +2,34 @@
 using namespace std;
 
 int main() {
-    cout << "Pattern like:\n   1\n  121\n 12321\n1234321 (while loop)";
-    int n = 4, row = 1;
+    cout << "Pattern like:\n1234554321\n1234**4321\n123****321\n12******21\n1********1 (while loop)";
+    int n = 5, row = 1;
     cout << "\nOutput\n";
     while (row <= n) {
-        int space = n-row;  // Start with 1 space in each row
-        while (space>0) { // Loop for spaces
-            cout << " ";
-            space--;
-        }
-        int col=1;
-        while (col<=row)
-        {
-            cout<<col;
-            col++;
-        }
         int col1=1;
-        while (col1<row)
+        while (col1<=n-row+1)
         {
-            cout<<row-col1;
+            cout<<col1;
             col1++;
         }
-        
+        int space1=0;
+        while(space1<=row-2)
+        {
+            cout<<"*";
+            space1++;
+        }
+        int space2=0;
+        while (space2<=row-2)
+        {
+            cout<<"*";
+            space2++;
+        }
+        int col2=n-row+1;
+        while(col2>0)
+        {
+            cout<<col2;
+            col2--;
+        }     
         row++;
         cout << endl;
     }
