16a1bff0a85557aea5ff6c5f10a0c95f9bc5e5d4
an <atianaim40@gmail.com>
2024-05-17 23:38:28 +0530

    Fibonacci

--- Initial.cpp
+++ Initial.cpp
@@ -1,38 +1,14 @@
+// FIBONACCI SERIES
+
 #include <iostream>
 using namespace std;
-
-int main() {
-    cout << "Pattern like:\n1234554321\n1234**4321\n123****321\n12******21\n1********1 (while loop)";
-    int n = 5, row = 1;
-    cout << "\nOutput\n";
-    while (row <= n) {
-        int col1=1;
-        while (col1<=n-row+1)
-        {
-            cout<<col1;
-            col1++;
-        }
-        int space1=0;
-        while(space1<=row-2)
-        {
-            cout<<"*";
-            space1++;
-        }
-        int space2=0;
-        while (space2<=row-2)
-        {
-            cout<<"*";
-            space2++;
-        }
-        int col2=n-row+1;
-        while(col2>0)
-        {
-            cout<<col2;
-            col2--;
-        }     
-        row++;
-        cout << endl;
+int main(){
+    int num=10,sum=1;
+    cout<<"0"<<endl;
+    for (int i = 0; i < num; i++)
+    {
+        cout<<sum<<endl;
+        sum=sum+i;
     }
     return 0;
 }
-
