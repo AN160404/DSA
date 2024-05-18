b34ca8a5390f1bd7a3afc8fa9374d4ab3931127d
an <atianaim40@gmail.com>
2024-05-18 14:10:40 +0530

    // #Leetcode 1281

--- Initial.cpp
+++ Initial.cpp
@@ -1,14 +1,34 @@
-// FIBONACCI SERIES
-
+// #Leetcode 1281
 #include <iostream>
 using namespace std;
-int main(){
-    int num=10,sum=1;
-    cout<<"0"<<endl;
-    for (int i = 0; i < num; i++)
-    {
-        cout<<sum<<endl;
-        sum=sum+i;
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int sum=0,product=1,diff;
+        int num=n,num1=n;
+        int rem=1;
+        int len=0;
+        while(num1>0)
+        {
+            num1=num1/10;
+            len++;
+            cout<<len<<endl;
+        }
+        for(int i=0;i<len;i++)
+        {
+            sum=sum+num%10;
+            product=product*(num%10);
+            num=num/10;
+        }
+        diff=product-sum;
+        cout<<"product"<<product<<endl;
+        cout<<"sum"<<sum<<endl;
+        cout<<"diff:"<<diff<<endl; 
     }
-    return 0;
+};
+int main()
+{
+    Solution obj;
+    obj.subtractProductAndSum(111);
 }
