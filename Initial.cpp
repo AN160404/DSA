739c78396383a6c91616399384ae00c8f2326639
an <atianaim40@gmail.com>
2024-05-31 15:40:29 +0530

    // Decimal to binary of negative number with bits class

--- Initial.cpp
+++ Initial.cpp
@@ -1,26 +1,16 @@
-// #Leetcode 191
+// Decimal to binary of negative number with bits class
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
-    int hammingWeight(int n) {
+    int negativenum(int n) {
         
-        int count=0;
-
-        while(n!=0)
-        {
-            if (n&1)
-            {
-                count++;
-            }
-            n=n>>1;
-        }
+        cout<<bitset<32>(n);
     }
 };
-
 int main()
 {
     Solution obj;
-    obj.hammingWeight(1);
+    obj.negativenum(-128);
 }
