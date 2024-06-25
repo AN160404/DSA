08fa4ff85686af9db4aeecb429c68943ba5115d8
an <atianaim40@gmail.com>
2024-06-25 15:22:24 +0530

    //Deletion in an Array by position

--- Array.cpp
+++ Array.cpp
@@ -1,4 +1,4 @@
-//Deletion in an Array by number
+//Deletion in an Array by position
 #include <iostream>
 using namespace std;
 int main()
@@ -6,18 +6,18 @@ int main()
     int size=5;
     int arr[size]={1,2,3,4,5};
     int num=0,pos=0;
-    cout<<"Enter number to be deleted:"<<endl;
-    cin>>num;
+    cout<<"Enter position to be deleted:"<<endl;
+    cin>>pos;
 
-    for (int i = size; i >=0; i--)
+    for (int i =pos; i <size; i++)
     {
-        if(num==arr[i])
-        {
-            arr[i]=0;
-        }
+        arr[i]=arr[i+1];
     }
+    size--;
+    cout<<"deleted list --"<<endl;
     for (int i = 0; i < size; i++)
     {
+        
         cout<<arr[i]<<endl;
     }
     
