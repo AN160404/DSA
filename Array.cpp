1e7b2de5ac1270ce3ad81228a38af6c91e7d2c5e
an <atianaim40@gmail.com>
2024-06-25 15:10:52 +0530

    //Deletion in an Array by number

--- Array.cpp
+++ Array.cpp
@@ -1,4 +1,4 @@
-//Insertion in an Array
+//Deletion in an Array by number
 #include <iostream>
 using namespace std;
 int main()
@@ -6,17 +6,16 @@ int main()
     int size=5;
     int arr[size]={1,2,3,4,5};
     int num=0,pos=0;
-    cout<<"Enter number to be inserted:"<<endl;
+    cout<<"Enter number to be deleted:"<<endl;
     cin>>num;
-    cout<<"Enter where to be inserted"<<endl;
-    cin>>pos;
-    size++;
 
-    for (int i = size; i >=pos; i--)
+    for (int i = size; i >=0; i--)
     {
-        arr[i+1]=arr[i];
+        if(num==arr[i])
+        {
+            arr[i]=0;
+        }
     }
-    arr[pos]=num;
     for (int i = 0; i < size; i++)
     {
         cout<<arr[i]<<endl;
