f836cc21e0bfe691584dd25a305d30f3ad7a6b88
an <atianaim40@gmail.com>
2024-06-25 14:55:45 +0530

    Insertion in an Array

--- Array.cpp
+++ Array.cpp
@@ -1,20 +1,25 @@
-//Traversing in an Array
+//Insertion in an Array
 #include <iostream>
 using namespace std;
 int main()
 {
-    int arr[5]={1,2,3,4,5};
-    int num=0;
-    cout<<"Enter number to be traversed:"<<endl;
+    int size=5;
+    int arr[size]={1,2,3,4,5};
+    int num=0,pos=0;
+    cout<<"Enter number to be inserted:"<<endl;
     cin>>num;
-    for (int i = 0; i < sizeof(arr); i++)
+    cout<<"Enter where to be inserted"<<endl;
+    cin>>pos;
+    size++;
+
+    for (int i = size; i >=pos; i--)
     {
-        if(num == arr[i])
-        {
-            cout<<"Number exists at "<<i<<endl;
-            return 0;
-        }
-        
+        arr[i+1]=arr[i];
     }
-    return 0;
+    arr[pos]=num;
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
 }
