f47131685ff940d4de1f196612bd2f9e61b01714
an <atianaim40@gmail.com>
2024-07-09 16:04:59 +0530

    Creation with OOPs

--- Linked-List.cpp
+++ Linked-List.cpp
@@ -1,61 +1,21 @@
 #include <iostream>
 using namespace std;
-struct Node{
+
+class Node{
+    public:
     int data;
-    struct Node * next;
+    Node* next;
+
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
 };
 int main()
 {
-    struct Node* head,*second,*third,*fourth,*fifth;
-    head=(struct Node*) malloc(sizeof(struct Node));
-    second=(struct Node*) malloc(sizeof(struct Node));
-    third=(struct Node*) malloc(sizeof(struct Node));
-    fourth=(struct Node*) malloc(sizeof(struct Node));
-    fifth=(struct Node*) malloc(sizeof(struct Node));
+    Node *head= new Node(6);
+    cout<<head->data<<endl;
+    cout<<head->next<<endl;
 
-    head->data=0;
-    head->next=second;
-    second->data=1;
-    second->next=third;
-    third->data=2;
-    third->next=fourth;
-    fourth->data=3;
-    fourth->next=fifth;
-    fifth->data=4;
-    fifth->next=NULL;
-    
-    struct Node *ptr1;
-    ptr1=head;
-    cout<<"Old List:"<<endl;
-
-    while (ptr1!=NULL)
-    {
-        cout<<ptr1->data<<endl;
-        ptr1=ptr1->next;
-    }    
-
-    struct Node *ptr2;
-    ptr2=head;
-
-    int i=0,loc=0;
-    cout<<"Enter location:"<<endl;
-    cin>>loc;
-
-    while (i<loc-1)
-    {
-        ptr2=ptr2->next;
-        i++;
-    }
-    ptr2->next=ptr2->next->next;
-
-    struct Node *ptr3;
-    ptr3=head;
-    while (ptr3!=NULL)
-    {
-        cout<<ptr3->data<<endl;
-        ptr3=ptr3->next;
-    }
-    
-    
-    return 0;
 }
