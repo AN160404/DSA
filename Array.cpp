c08160a0b24d0a828a76a0a1c8f7c925271009d3
an <atianaim40@gmail.com>
2024-09-16 15:56:17 +0530

    Counting Sort in O(n)

--- Array.cpp
+++ Array.cpp
@@ -1,49 +1,37 @@
-<<<<<<< HEAD
-//duplicate zeros
-#include <stdio.h>
-#include <vector>
+//Counting Sort O(n)
+#include <iostream>
 using namespace std;
-class Solution 
+class Algo
 {
     public:
-    void duplicatezeros(vector<int>& arr)
-    {
-        int length=arr.size();
-        for(int i=0;i<length-1;i++)
+    void countingSort(int a[],int n,int k){
+        int b[n]={0};
+        int count[k+1]={0};
+
+        for (int i = 0; i < n; i++)
         {
-            if(arr[i]==0){
-                for(int j=length-1;j>i;j--)
-                {
-                    arr[j]=arr[j-1];
-                }
-                i++;
-            }
+            count[a[i]]++;
+        }
+        for(int i=1;i<=k;i++)
+        {
+            count[i]=count[i]+count[i-1];
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            count[a[i]]--;
+            b[count[a[i]]]=a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            a[i]=b[i];
+            cout<<a[i]<<endl;
         }
     }
 };
-=======
-//2D matrix to 1D by Row major order
-#include <iostream>
-using namespace std;
 int main()
 {
-    int rows=3,columns=3;
-    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int index=0;
-    int flattened[rows*columns]={};
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            flattened[index]=arr[i][j];
-            index++;
-        }
-    }
-    for (int i = 0; i < rows*columns; i++)
-    {
-        cout<<flattened[i]<<endl;
-    }
-    
-    return 0;
+    Algo obj;
+    int n=7,k=4;
+    int a[n]={2,1,2,3,1,2,4};
+    obj.countingSort(a,n,k);
 }
->>>>>>> 0b22661e92fcbd67e1b475016877aa22e5cd371b
