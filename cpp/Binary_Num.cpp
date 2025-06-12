ecd9a1b62daaf78fbf4355d1f3c35b772e0b03e2
AN160404 <atianaim40@gmail.com>
2025-06-12 18:05:22 +0530

    reversebits

--- cpp/Binary_Num.cpp
+++ cpp/Binary_Num.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int findComplement(int num) {
-        int result = 0;
-        int power = 1;
-        while (num > 0) {
-            int bit = num % 2;
-            num /= 2;
-            result += (1 - bit) * power;
-            power *= 2;
+uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            int lastBit = n % 2;       // Get the last bit (0 or 1)
+            result = result * 2 + lastBit; // Shift result left and add the bit
+            n = n / 2;                 // Shift input right
         }
+
         return result;
     }
 
-int main(){
-    int result=findComplement(5);
+uint32_t main(){
+    uint32_t result=reverseBits(00000010100101000001111010011100);
     cout<<result<<endl;
+    return 0;
 }
