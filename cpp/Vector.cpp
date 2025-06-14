979a5fe35c5cded7a8dcc75a399b5e7768d56434
AN160404 <atianaim40@gmail.com>
2025-06-14 19:54:35 +0530

    Reversing Vector Array

--- cpp/Vector.cpp
+++ cpp/Vector.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-
+//reversing the array
 int main(){
     vector<int> nums={1,2,3,4,5};
-    int n=3;
-    for(int val:nums){
-        if(n==val){
-            cout<<n<<" is present"<<endl;
-        }
+    int start=0, end=nums.size()-1;
+    while(start<end){
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
     }
+    for (int i : nums)
+    {
+        cout<<i<<endl;
+    }
+    
     return 0;
 }
