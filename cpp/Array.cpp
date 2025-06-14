bcbdf8c0e2d2500d7bfd331ee0f83a286a2929ab
AN160404 <atianaim40@gmail.com>
2025-06-14 15:13:49 +0530

    largest number

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
     int arr[10]={12,33,4,5,6,12,34,32,2,1};
     int len=sizeof(arr)/sizeof(arr[0]);
-    int num=INT16_MAX;
+    int num=INT16_MIN;
 
     for (int i = 1; i <len ;i++){
-        if(num>arr[i]){
+        if(num<arr[i]){
             num=arr[i]; 
         }
     }
 
-    cout<<num<<" is the smallest number."<<endl;
+    cout<<num<<" is the largest number."<<endl;
     return 0;
 }
