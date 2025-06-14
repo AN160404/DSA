4b0ccdfe0593f65552b77e7f734af38d69718864
AN160404 <atianaim40@gmail.com>
2025-06-14 15:09:08 +0530

    Smallest number

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -1,51 +1,17 @@
-//Counting Sort O(n)
 #include <iostream>
-#include <string>
 using namespace std;
-class Algo
-{
-    public:
-    int getMax(int a[],int n)
-    {
-        size_t max_length = 0;
-        for (int i = 0; i < n; ++i) {
-            size_t length = to_string(a[i]).length();
-            if (length > max_length) {
-                max_length = length;
-            }
+
+int main(){
+    int arr[10]={12,33,4,5,6,12,34,32,2,1};
+    int len=sizeof(arr)/sizeof(arr[0]);
+    int num=INT16_MAX;
+
+    for (int i = 1; i <len ;i++){
+        if(num>arr[i]){
+            num=arr[i]; 
         }
-    return max_length;
-
     }
-    void radixSort(int a[],int n){
-        int b[n]={0};
-        int count[10]={0};
-        int max=getMax(a,n);
 
-        // for (int i = 0; i < n; i++)
-        // {
-        //     count[a[i]]++;
-        // }
-        // for(int i=1;i<=k;i++)
-        // {
-        //     count[i]=count[i]+count[i-1];
-        // }
-        // for(int i=n-1;i>=0;i--)
-        // {
-        //     count[a[i]]--;
-        //     b[count[a[i]]]=a[i];
-        // }
-        // for (int i = 0; i < n; i++)
-        // {
-        //     a[i]=b[i];
-        //     cout<<a[i]<<endl;
-        }    
-};
-
-int main()
-{
-    Algo obj;
-    int n=7;
-    int a[n]={2,1,2,3,1,2,4234};
-    obj.radixSort(a,n);
+    cout<<num<<" is the smallest number."<<endl;
+    return 0;
 }
