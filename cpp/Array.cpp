d172cbe8c14943cbd1c49e6197ef70f37f7c771a
AN160404 <atianaim40@gmail.com>
2025-06-14 17:17:51 +0530

    Sum and Product of Numbers in an Array

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
-
+//Sum and Product of all numbers
 int main(){
     int arr[5]={1,2,3,4,5};
     int len=sizeof(arr)/sizeof(arr[0]);
-    int num=5;
-    int temp=0;
-    int start=0,end=len-1;
+    int sum=0,product=1;
+    int start=0;
 
-    while (start<end){
-        temp=arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
-        start++;
-        end--;
-    }
     for (int i = 0; i < len; i++)
     {
-        cout<<arr[i]<<endl;
+        sum+=arr[i];
+        product*=arr[i];
     }
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"Product: "<<product<<endl;
     
     return 0;
 }
