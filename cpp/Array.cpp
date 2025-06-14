fb4d68b46afcbb9874471b338baa6b4cf38ec4bb
AN160404 <atianaim40@gmail.com>
2025-06-14 15:17:18 +0530

    Linear Search

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main(){
     int arr[10]={12,33,4,5,6,12,34,32,2,1};
     int len=sizeof(arr)/sizeof(arr[0]);
-    int num=INT16_MIN;
+    int num=5;
 
     for (int i = 1; i <len ;i++){
-        if(num<arr[i]){
-            num=arr[i]; 
+        if(num==arr[i]){
+            cout<<i<<" is the position of the element."<<endl; 
         }
     }
 
-    cout<<num<<" is the largest number."<<endl;
     return 0;
 }
