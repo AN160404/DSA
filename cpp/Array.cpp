fdeda046c6020673a912a3404cbc55e615ed2ec0
AN160404 <atianaim40@gmail.com>
2025-06-15 23:35:11 +0530

    Pair Sum (Brute Force)

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 #include <vector>
 using namespace std;
-
 int main(){
-    vector<int> vec={-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> vec={1,2,3,4,5,11};
     int count=vec.size();
-    int currsum=0;
-    int maxsum=INT16_MIN;
+    int target=9;
+    vector<int> element;
     for (int i = 0; i < count; i++)
     {
-        currsum+=vec[i];
-        maxsum=max(maxsum,currsum);
-        if(currsum<0){
-            currsum=0;
+        for (int j = i+1; j < count; j++)
+        {
+            if(vec[i]+vec[j] ==target)
+            {
+                element.push_back(vec[i]);
+                element.push_back(vec[j]);
+            }
         }
     }
-    cout<<maxsum<<endl;    
+    cout<<element[0]<<" "<<element[1]<<endl;
+    return 0; 
 }
