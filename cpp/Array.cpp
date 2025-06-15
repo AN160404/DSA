1ffd11c527d6a23b720a99c1ccc5f85f31409ea6
AN160404 <atianaim40@gmail.com>
2025-06-15 22:41:23 +0530

    Maxsum of Subarrays (Kadane) Leetcode 53

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -3,19 +3,17 @@
 using namespace std;
 
 int main(){
-    vector<int> vec={1,2,3,4,5};
+    vector<int> vec={-2,1,-3,4,-1,2,1,-5,4};
     int count=vec.size();
+    int currsum=0;
     int maxsum=INT16_MIN;
     for (int i = 0; i < count; i++)
     {
-        int currsum=0;
-        for (int j = i; j < count; j++)
-        {
-            currsum+=vec[j];
-            maxsum=max(maxsum,currsum);
+        currsum+=vec[i];
+        maxsum=max(maxsum,currsum);
+        if(currsum<0){
+            currsum=0;
         }
     }
-    cout<<maxsum<<endl;
-
-    
+    cout<<maxsum<<endl;    
 }
