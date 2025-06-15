3dfce23f0c5b80a88d0330347672ef1b48a1342a
AN160404 <atianaim40@gmail.com>
2025-06-15 22:07:34 +0530

    Printing Subarrays

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -1,34 +1,21 @@
-#include <iostream>
+#include<iostream>
 #include <vector>
 using namespace std;
 
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        
-        int start=0,end=nums.size()-1;
-
-        while(start<=end){
-        int mid=start+(end-start)/2;
-        if(target==nums[mid]){
-            return mid;
+int main(){
+    vector<int> vec={1,2,3,4,5};
+    int count=vec.size();
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = i; j < count; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout<<vec[k];
+            }
+            cout<<" ";
         }
-        else if(target>nums[mid]){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
-        }
-        return -1;
+        cout<<endl;
     }
-};
-
-int main()
-{
-    vector<int> vec={1,2,3,4,5,6,7,8,9};
-    Solution obj;
-    int result=obj.search(vec,6);
-    cout<<"Position: "<<result<<endl;
-    return 0;
+    
 }
