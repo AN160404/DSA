42fc16f5709db290c37050c7249eab2146be676a
AN160404 <atianaim40@gmail.com>
2025-06-16 15:11:42 +0530

    Majority Element by Sorting

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main(){
-    vector<int> vec={1,2,0,2,1};
-    int count=vec.size();
-    int maj=0;
+    vector<int> nums={1,2,2,2,1};
+    int count=nums.size();
+    int maj=nums[0];
     bool ismaj=true;
+    int freq=1;
+    sort(nums.begin(),nums.end());
     for (int i = 0; i < count; i++)
     {
-        int num=0;
-        for (int j = 0; j < count; j++)
+        if (nums[i]==nums[i-1])
         {
-            if(vec[i]==vec[j])
-            {
-                num++;
-                if(num>count/2){
-                    ismaj=true;
-                    maj=vec[i];
-                }
-                else{
-                    ismaj=false;
-                }
-            }
+            freq++;
         }
-        
+        else{
+            freq=1;
+            maj=nums[i];
+        }
+        if(freq>count/2){
+            maj=nums[i];
+            ismaj=true;
+        }
+        else{
+            ismaj=false;
+        }
     }
+    
     if (ismaj)
     {
         cout<<"Majority Element: "<<maj<<endl;
