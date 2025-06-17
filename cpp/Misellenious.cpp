64de60087c49901f0a24541b26d68829e9037b07
AN160404 <atianaim40@gmail.com>
2025-06-17 19:38:09 +0530

    Power

--- cpp/Misellenious.cpp
+++ cpp/Misellenious.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n){
-    int fact=1;
-    for (int i = 1; i <= n; i++)
-    {
-        fact *= i;
+int main(){
+    long pow=10;
+    int n=2;
+    double ans=1;
+    while(pow>0){
+        if(pow%2==1){
+            ans=ans*n;
+        }
+        n=n*n;
+        pow=pow/2;
     }
-    return fact;
-}
-int main()
-{
-    int n,r,a;
-    cout<<"Enter n:"<<endl;
-    cin>>n;
-    cout<<"Enter r:"<<endl;
-    cin>>r;
-
-    int fact_n=factorial(n);
-    int fact_r=factorial(r);
-    int fact_nr=factorial(n-r);
-    a=(fact_n/(fact_r*fact_nr));
-    cout<<a<<endl;
-    return 0;
+    cout<<ans<<endl;
 }
