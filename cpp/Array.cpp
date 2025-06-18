d60ef88580405f5122f90e90438fb69f645b5afe
AN160404 <atianaim40@gmail.com>
2025-06-18 20:11:44 +0530

    Container With Most Water Optimized

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main(){
     vector<int> height={1,8,6,2,5,4,8,3,7};
     int size=height.size();
-        int maxwater=0;
-        for(int i=0;i<size;i++){
-            for(int j=i;j<size;j++){
-                int w=j-i;
-                int h=min(height[i],height[j]);
-                maxwater=max(maxwater,w*h);
-            }
-            
-        }
+    int maxwater=0;
+    int l=0,r=size-1;
+    while(l<r){
+        int w=r-l;
+        int h=min(height[l],height[r]);
+        int area=w*h;
+        maxwater=max(maxwater,area);
+        height[l]<height[r] ? l++ : r--;
+    }
     cout<<maxwater<<endl;
 }
