7de2c5c08d80eaccce864052c6aa36e4c90653d4
AN160404 <atianaim40@gmail.com>
2025-06-19 23:43:57 +0530

    Single Element in a Sorted Array (Leetcode 540)

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -5,22 +5,40 @@ using namespace std;
 
 
 int main(){
-    vector<int> arr={1,2,7,3};
-    int size=arr.size();
-    int st=1,end=size-2;
-        
-    while(st<=end){
-            int mid=st+(end-st)/2;
-            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
-                cout<<mid;
-                return mid;
+    vector<int> nums={1,1,2,2,3,7,7};
+    int size=nums.size();
+    int st=0,end=size-1;
+        int mid;
+        if(size==1){
+            return nums[0]; // For array of size 1
+        }
+        while(st<=end){
+
+            mid=st+(end-st)/2;
+            if(mid==0 && nums[0]!=nums[1]) return nums[0]; //Handling 1st element
+            if(mid==size-1 && nums[size-1]!=nums[size-2]) return nums[size-1]; //Handling last element
+
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+
+
+            if(mid%2==0){  //For {1,1,2,3,3,4,4,8,8}  On dividing by two even number of elements on each side thus mid also even
+                if(nums[mid]==nums[mid-1]){
+                    end=mid-1;
+                }
+                else{
+                    st=mid+1;
+                }
                 
             }
-            else if(arr[mid]>arr[mid-1]){    //Mid is on the left side of the mountain.
-                st=mid+1;  
-            }
-            else{ //Mid is on the right side of the mountain
-                end=mid-1;
+            else{  // For {3,3,7,7,10,11,11}  On dividing by two odd number of elements on each side thus mid also odd
+                if(nums[mid]==nums[mid-1]){
+                    st=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
             }
         }
         return 0;
