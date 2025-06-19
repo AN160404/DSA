03f29887722447bab8f447f520718f1cc94ed5dc
AN160404 <atianaim40@gmail.com>
2025-06-19 17:38:52 +0530

    Searching in a Rotated Sorted Array (Leetcode 33)

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -2,30 +2,41 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st=0,end=nums.size()-1;
+        while(st<=end)
+        {int mid=st+(end-st)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[st]<=nums[mid]){    //Left Part is Sorted
+            if(target>=nums[st] && target<=nums[mid]){ // working on left part of main array
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        else{  //Right part is sorted
+        if(target>=nums[mid] && target<=nums[end]){ // working on right part of sorted array
+            st=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        }}
+        return -1;
+    }
+};
 int main(){
-    vector<int> nums={1,2,3,4};
-    int size=nums.size();
+    vector<int> nums={1,2,3,4,7,0};
+    int target=7;
+    Solution obj;
+    int result=obj.search(nums,target);
+    cout<<result;
     
-    // we divide into 2 parts suffix and prefix then multiply the elements of 2 vectors.
-    vector<int> ans(size,1);
-
-    // from the start the answer will be stored as {1,1,2,6} 
-    for(int i=1;i<size;i++){
-        ans[i]=ans[i-1]*nums[i-1];
-    }
-
-    //from the end we multiply the suffix with ans array. Answer will be as:-
-    // For i=2: {1,1,2,6}  suffix=1*4=4  =>ans[2]*suffix=8  ==>{1,1,8,6}
-    // For i=1: {1,1,8,6}  suffix=4*3=12 =>ans[1]*suffix=12 ==>{1,12,8,6}
-    // For i=0: {1,12,8,6} suffix=12*2   =>ans[0]*suffix=24 ==>{24,12,8,6}
-    int suffix=1;
-    for(int i=size-2;i>=0;i--){
-        suffix=suffix*nums[i+1];
-        ans[i]=ans[i]*suffix;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        cout<<ans[i]<<endl;
-    }
     
 }
