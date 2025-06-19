c1ec4e757a035e6451dae7db910ca85f05d8402b
AN160404 <atianaim40@gmail.com>
2025-06-19 23:19:33 +0530

    Peak Index in a Mountain Array (Optimized)

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -5,12 +5,22 @@ using namespace std;
 
 
 int main(){
-    vector<int> arr={1,2,7,3,1,0};
+    vector<int> arr={1,2,7,3};
     int size=arr.size();
-        for(int i=1;i<=size-1;i++){
-            if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
-                cout<<i<<endl;
-                return i;
+    int st=1,end=size-2;
+        
+    while(st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                cout<<mid;
+                return mid;
+                
+            }
+            else if(arr[mid]>arr[mid-1]){    //Mid is on the left side of the mountain.
+                st=mid+1;  
+            }
+            else{ //Mid is on the right side of the mountain
+                end=mid-1;
             }
         }
         return 0;
