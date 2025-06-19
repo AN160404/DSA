a50ab280d5ab72aebcc0dacfe772819441ff7d70
AN160404 <atianaim40@gmail.com>
2025-06-20 02:15:20 +0530

    !!! Allocate Minimum Pages (GFG)

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -3,44 +3,52 @@
 #include <algorithm>
 using namespace std;
 
+int isValid(vector<int> &arr,int size,int n,int mid){
+    int student=1,pagecount=0;
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i]>mid){
+            return false;
+        }
+        if(pagecount+arr[i]<=mid){
+            pagecount+=arr[i]; //Adding until it's coming less
+        }
+        else{
+            student++; //Increasing the student count just when the page count exceeds the mid or maxallowed
+            pagecount=arr[i]; //Adding the page that couldn't be added into the last student.
+        }
+    }
+    if(student>n){
+        return false;
+    }
+    else{
+        return true;
+    }
+    
+}
 
 int main(){
-    vector<int> nums={1,1,2,2,3,7,7};
-    int size=nums.size();
-    int st=0,end=size-1;
-        int mid;
-        if(size==1){
-            return nums[0]; // For array of size 1
+    vector<int> arr={2,1,3,4};
+    int size=arr.size();
+    int n=2;
+    int st=0,end=0;
+    int ans=0;
+    for(int val:arr){
+        end+=val;
+    }
+    int mid=0;
+    while(st<=end){
+        mid=st+(end-st)/2;
+        if(isValid(arr,size,n,mid)){
+            ans=mid;
+            end=mid-1;
         }
-        while(st<=end){
-
-            mid=st+(end-st)/2;
-            if(mid==0 && nums[0]!=nums[1]) return nums[0]; //Handling 1st element
-            if(mid==size-1 && nums[size-1]!=nums[size-2]) return nums[size-1]; //Handling last element
-
-            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
-                return nums[mid];
-            }
-
-
-            if(mid%2==0){  //For {1,1,2,3,3,4,4,8,8}  On dividing by two even number of elements on each side thus mid also even
-                if(nums[mid]==nums[mid-1]){
-                    end=mid-1;
-                }
-                else{
-                    st=mid+1;
-                }
-                
-            }
-            else{  // For {3,3,7,7,10,11,11}  On dividing by two odd number of elements on each side thus mid also odd
-                if(nums[mid]==nums[mid-1]){
-                    st=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
+        else{
+            st=mid+1;
         }
+    }
+    cout<<ans;
+
         return 0;
     
     
