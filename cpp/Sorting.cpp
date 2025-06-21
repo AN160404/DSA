29fac2855f506663c4357c4e0ce5bb074688c50c
AN160404 <atianaim40@gmail.com>
2025-06-21 22:32:43 +0530

    Insertion Sort

--- cpp/Sorting.cpp
+++ cpp/Sorting.cpp
@@ -5,20 +5,18 @@ using namespace std;
 int main(){
     vector<int> nums={4,1,5,2,3};
 
-    for(int i=0;i<nums.size()-1;i++){
-        // Inititalizing the minimum element
-        int min=i;
-        for (int j = i+1; j < nums.size(); j++)
-        {
-            if(nums[j]<nums[min]){
-                min=j; // Minimum changed
-            }
+    for(int i=1;i<nums.size();i++){
+        // Inititalizing the current element
+        int curr=nums[i];
+        // Initializing the previous positions
+        int prev=i-1;
+        // Traverse all previous until they come to be less than current value.
+        while(prev>=0 && nums[prev]>curr){
+            nums[prev+1]=nums[prev]; // Shifting all the previous with one step
+            prev--;
         }
-
-        //Swapping the minimum element with the previous minimum element
-        int temp=nums[min];
-        nums[min]=nums[i];
-        nums[i]=temp;        
+        // Redefine the number to be switched with the current value
+        nums[prev+1]=curr; // +1 because if we reach a number less than curr, we replace the number next to it with current.
     }
 
             for (int i = 0; i < nums.size(); i++)
