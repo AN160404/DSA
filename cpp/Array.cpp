b755af78ab8eec3ddc97965dc72445be277ef50f
AN160404 <atianaim40@gmail.com>
2025-06-21 22:55:37 +0530

    Sort Colors (optimised)

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -6,7 +6,29 @@ using namespace std;
 
 int main(){
     vector<int> nums={2,0,1,1,0,1,2,0,0};
-    sort(nums.begin(),nums.end());
+    int size=nums.size();
+        int zero=0,two=0,one=0;
+        for(int i=0;i<size;i++){
+            if(nums[i]==0){
+                zero++;
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+            else if(nums[i]==2){
+                two++;
+            }
+        }
+        int idx=0;
+        for(int i=0;i<zero;i++){
+            nums[idx++]=0;
+        }
+        for(int i=0;i<one;i++){
+            nums[idx++]=1;
+        }
+        for(int i=0;i<two;i++){
+            nums[idx++]=2;
+        }
     return 0;
     
 }
