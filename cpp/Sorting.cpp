65179ed49352f0108523349a677e274e8ca01fdf
AN160404 <atianaim40@gmail.com>
2025-06-21 20:41:39 +0530

    Selection Sort

--- cpp/Sorting.cpp
+++ cpp/Sorting.cpp
@@ -4,30 +4,26 @@ using namespace std;
 
 int main(){
     vector<int> nums={4,1,5,2,3};
-    bool isSwap=false;
-    for(int i=0;i<nums.size();i++){
+
+    for(int i=0;i<nums.size()-1;i++){
+        // Inititalizing the minimum element
+        int min=i;
         for (int j = i+1; j < nums.size(); j++)
         {
-            isSwap=false;
-            if (nums[i]>nums[j])
-            {
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                isSwap=true;
+            if(nums[j]<nums[min]){
+                min=j; // Minimum changed
             }
         }
-        
+
+        //Swapping the minimum element with the previous minimum element
+        int temp=nums[min];
+        nums[min]=nums[i];
+        nums[i]=temp;        
     }
-    if (isSwap==false){
-        cout<<"Array already sorted";
-    }
-    else
-    {
+
             for (int i = 0; i < nums.size(); i++)
     {
         cout<<nums[i]<<endl;   
-    }
     }
     
 
