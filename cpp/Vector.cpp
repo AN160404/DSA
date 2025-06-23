b1e59bdc1cfbc13f6d9c594a40efbe30946785ec
AN160404 <atianaim40@gmail.com>
2025-06-24 01:42:42 +0530

    Basic Functions

--- cpp/Vector.cpp
+++ cpp/Vector.cpp
@@ -3,19 +3,28 @@
 using namespace std;
 //reversing the array
 int main(){
-    vector<int> nums={1,2,3,4,5};
-    int start=0, end=nums.size()-1;
-    while(start<end){
-        int temp=nums[start];
-        nums[start]=nums[end];
-        nums[end]=temp;
-        start++;
-        end--;
-    }
-    for (int i : nums)
+    vector<int> vec={1,2,3,4,5};
+    // Erasing nums in a range (1 to 4)
+    vec.erase(vec.begin()+1,vec.begin()+3);
+
+    // Inserting at a position
+    vec.insert(vec.begin()+1,100);
+
+    // Clearing the whole array
+    vec.clear();
+
+    // Checking if the vector array is empty
+    int ans=vec.empty();
+
+    cout<<ans<<endl;
+
+    
+    for (int i : vec)
     {
         cout<<i<<endl;
     }
+
+
     
     return 0;
 }
