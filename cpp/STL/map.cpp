3483404b1fc34f2e846171e617d337009c701d92
AN160404 <atianaim40@gmail.com>
2025-06-24 03:04:58 +0530

    Unordered Map

--- cpp/STL/map.cpp
+++ cpp/STL/map.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
-// Multi-Map -- Same values can be added into this
-// m[key]=value -- notation is not allowed in this
+// Unordered-map -- duplicate values can't be stored
+// Stored in a stack like format 
+// O/P: C:20
+// B:20
+// A:20
 
 int main(){
-    multimap<string,int> m;
+    unordered_map<string,int> m;
     
     m.emplace("A",20);
-    m.emplace("A",20);
-    m.emplace("A",20);
+    m.emplace("B",20);
+    m.emplace("C",20);
 
 
     for(auto val:m){
