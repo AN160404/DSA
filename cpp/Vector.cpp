db78619bf52f360b6afa1eac2e13d8805fd463ca
AN160404 <atianaim40@gmail.com>
2025-06-24 01:55:06 +0530

    Creating an Iterator

--- cpp/Vector.cpp
+++ cpp/Vector.cpp
@@ -4,27 +4,11 @@ using namespace std;
 //reversing the array
 int main(){
     vector<int> vec={1,2,3,4,5};
-    // Erasing nums in a range (1 to 4)
-    vec.erase(vec.begin()+1,vec.begin()+3);
-
-    // Inserting at a position
-    vec.insert(vec.begin()+1,100);
-
-    // Clearing the whole array
-    vec.clear();
-
-    // Checking if the vector array is empty
-    int ans=vec.empty();
-
-    cout<<ans<<endl;
-
     
-    for (int i : vec)
-    {
-        cout<<i<<endl;
+    vector<int>::iterator name;
+    for(name=vec.begin();name!=vec.end();name++){
+        cout<<*(name)<<endl;
     }
 
-
-    
     return 0;
 }
