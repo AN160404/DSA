eceac08b84df1cfcbc82274069211d38438b9cfc
AN160404 <atianaim40@gmail.com>
2025-06-24 20:29:04 +0530

    Inputting Character Array

--- cpp/CharArray_Strings.cpp
+++ cpp/CharArray_Strings.cpp
@@ -4,19 +4,14 @@
 using namespace std;
 int main(){
 
-    char str[]={'a','b','c','\0'}; // Ended with "/0"
-    str[0]='z'; // Value can be changed
+    char str[100];
+
+    // Inputting value
+
+    // cin.getline(str,len,delimiter)
+    cin.getline(str,100,'.'); //Stops taking in word when . sign is entered
+
     cout<<str<<endl;
-    cout<<strlen(str)<<endl;
-
-
-    char str1[]="abc";
-    // str1[]="a"; // Value can't be changed
-    // str1[0]="z"; // Can't changed this way as well
-    cout<<str1<<endl;
-    cout<<str1[2]<<endl; // Can be traversed like this
-
-
 
     return 0;
 }
