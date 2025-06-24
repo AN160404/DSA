16711d83fcda4aa87ef6494757091b751537224f
AN160404 <atianaim40@gmail.com>
2025-06-24 20:49:51 +0530

    Palindrome String

--- cpp/CharArray_Strings.cpp
+++ cpp/CharArray_Strings.cpp
@@ -4,15 +4,20 @@
 using namespace std;
 int main(){
 
-    char str[]="Hello";
-    int n=strlen(str);
-    int i=0,j=n-1;
+    string str="madam";
+    string str1=str;
+    int i=0,j=str.length()-1;
     while(i<j){
         swap(str[i],str[j]);
         i++;
         j--;
     }
-    cout<<str<<endl;
+    if(str==str1){
+        cout<<"Palindrome"<<endl;
+    }
+    else{
+        cout<<"Not Palindrome"<<endl;
+    }
 
     return 0;
 }
