6e8c39a93315b9a22cfa507476b0154f8053ee06
AN160404 <atianaim40@gmail.com>
2025-06-24 20:31:53 +0530

    String Class

--- cpp/CharArray_Strings.cpp
+++ cpp/CharArray_Strings.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 
 using namespace std;
 int main(){
 
-    char str[100];
-
-    // Inputting value
-
-    // cin.getline(str,len,delimiter)
-    cin.getline(str,100,'.'); //Stops taking in word when . sign is entered
-
+    string str="Hello World"; // Dynamic in nature and thus resize in nature
     cout<<str<<endl;
 
     return 0;
