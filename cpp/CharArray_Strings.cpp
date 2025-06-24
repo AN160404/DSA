3d31ba3b83de3e6d93eb34675052879756d38cde
AN160404 <atianaim40@gmail.com>
2025-06-24 20:38:07 +0530

    Basic String Functions

--- cpp/CharArray_Strings.cpp
+++ cpp/CharArray_Strings.cpp
@@ -4,8 +4,18 @@
 using namespace std;
 int main(){
 
-    string str="Hello World"; // Dynamic in nature and thus resize in nature
-    cout<<str<<endl;
+    string str="Hello";
+    string str1;
+
+    getline(cin,str1); // Inputting
+
+    cout<<str+str1<<endl; // Concatenation
+    cout<<(str==str1)<<endl; // Comparision
+    cout<<(str>str1)<<endl; // Greater?
+    cout<<str.length()<<endl; // Length
+
+    // All loops can be run similarly to normal array.
+
 
     return 0;
 }
