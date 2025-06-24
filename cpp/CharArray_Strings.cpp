85a850682792dd3b1ca84a859b75d1c00172292f
AN160404 <atianaim40@gmail.com>
2025-06-24 22:54:55 +0530

    Valid Palindrome (Leetcode 125)

--- cpp/CharArray_Strings.cpp
+++ cpp/CharArray_Strings.cpp
@@ -2,22 +2,40 @@
 #include <string.h>
 
 using namespace std;
-int main(){
 
-    string str="madam";
-    string str1=str;
-    int i=0,j=str.length()-1;
-    while(i<j){
-        swap(str[i],str[j]);
+bool isAlphanumeric(char ch){
+    if (ch >= '0' && ch <= '9' || tolower(ch) >= 'a' && tolower(ch) <= 'z')
+    {
+        return true;
+    }
+    return false;
+}
+int main()
+{
+    string s="A man, a plan, a canal: Panama";
+    int i = 0, j = s.length() - 1;
+
+    while (i < j)
+    {
+        if (!isAlphanumeric(s[i]))
+        {
+            i++;
+            continue;
+        }
+        if (!isAlphanumeric(s[j]))
+        {
+            j--;
+            continue;
+        }
+        if (tolower(s[i]) != tolower(s[j]))
+        {
+            cout<<"Not Palindrome"<<endl;
+        }
+
         i++;
         j--;
     }
-    if(str==str1){
-        cout<<"Palindrome"<<endl;
-    }
-    else{
-        cout<<"Not Palindrome"<<endl;
-    }
-
+    cout<<"Palindrome"<<endl;
     return 0;
 }
+
