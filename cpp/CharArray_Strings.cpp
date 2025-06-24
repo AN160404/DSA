4cb5084a2e8c2364e5f89f1e8a15fbcc228163d4
AN160404 <atianaim40@gmail.com>
2025-06-24 20:46:17 +0530

    Reversing a Character Array (Leetcode 344)

--- cpp/CharArray_Strings.cpp
+++ cpp/CharArray_Strings.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 int main(){
 
-    string str="Hello";
-    
-    int i=0,j=str.length()-1;
+    char str[]="Hello";
+    int n=strlen(str);
+    int i=0,j=n-1;
     while(i<j){
-        int temp=str[i];
-        str[i]=str[j];
-        str[j]=temp;
+        swap(str[i],str[j]);
         i++;
         j--;
     }
