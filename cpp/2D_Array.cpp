7dc80d2af9030c695766b938439e52cc109cf05b
AN160404 <atianaim40@gmail.com>
2025-06-26 21:07:16 +0530

    Diagonal Sum (Optimised)

--- cpp/2D_Array.cpp
+++ cpp/2D_Array.cpp
@@ -3,29 +3,24 @@
 
 using namespace std;
 
-int main(){
-    int arr[4][4]={
-        {1,2,3,4},
-        {5,6,7,8},
-        {9,10,11,12},
-        {13,14,15,16}
-    };
+int main()
+{
+    int arr[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
 
-        int sum=0;
-        int n=4;
+    int sum = 0;
+    int n = 4;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        sum += arr[i][i];
+        if (i != n - 1 - i)
         {
-            if(i==j){
-                sum+=arr[i][j];
-            }
-            else if(j==n-i-1){
-                sum+=arr[i][j];
-            }
+            sum += arr[i][n - 1 - i];
         }
-        
     }
-    cout<<sum<<endl;;
-    
+    cout << sum << endl;
+    ;
 }
