16350ccd776b0535f69ad819a4252cd96e97605d
AN160404 <atianaim40@gmail.com>
2025-06-27 21:08:12 +0530

    2 Sum (Leetcode 1) BF

--- cpp/Array.cpp
+++ cpp/Array.cpp
@@ -6,51 +6,19 @@ using namespace std;
 int main()
 {
     vector<int> nums = {1, 2, 3};
-    int n = nums.size();
-    int pivot = -1;
-    // Finding pivot element
-    for (int i = n - 2; i >= 0; i--)
-    {
-        if (nums[i] < nums[i + 1])
-        {
-            pivot = i;
-            break;
+    int target=5;
+    vector<int> arr;
+        for(int i=0;i<nums.size();i++){
+            int sum=0;
+            for(int j=i+1;j<nums.size();j++){
+                sum=nums[i]+nums[j];
+                if(sum==target){
+                    arr.push_back(i);
+                    arr.push_back(j);
+                }
+            }
         }
-    }
-
-    // If no pivot then no need of sorting thus returning reversed array
-    if (pivot == -1)
-    {
-        reverse(nums.begin(), nums.end());
+        cout<<arr[0]<<endl;
+        cout<<arr[1]<<endl;
         return 0;
-    }
-
-    // Swapping the pivot with it's successor
-    for (int i = n - 1; i >= 0; i--)
-    {
-        if (nums[i] > nums[pivot])
-        {
-            int temp = nums[i];
-            nums[i] = nums[pivot];
-            nums[pivot] = temp;
-            break;
-        }
-    }
-
-    // Reversing all the other decresing values
-    int i = pivot + 1, j = n - 1;
-    while (i <= j)
-    {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-        i++;
-        j--;
-    }
-
-    for (int val : nums)
-    {
-        cout << val << endl;
-    }
-    return 0;
 }
