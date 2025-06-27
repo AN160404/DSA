465b344a349999beeb8e5fbedc8e71d7d8a90595
AN160404 <atianaim40@gmail.com>
2025-06-27 23:20:03 +0530

    2 Sum using Hashing

--- cpp/Hashing.cpp
+++ cpp/Hashing.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std;
 int main(){
-    string s="abdeasewasdczsd";
-    int n=s.length();
-    int hash[26]={0};
+    vector<int> nums={2,7,11,15};
+    int target=9;
+
+    vector<int> ans;
+    unordered_map<int,int> map;
+    int n=nums.size();
     for(int i=0;i<n;i++){
-        hash[s[i]-'a']++;
+        int first=nums[i];
+        int second=target-first;
+        if(map.find(second)!=map.end()){
+            ans.push_back(first);
+            ans.push_back(second);
+        }
+        map[first]=i;
     }
-    cout<<"Frequency of d is: "<<hash['d'-'a'];
+    cout<<ans[0]<<endl;
+    cout<<ans[1]<<endl;
     
     
 }
