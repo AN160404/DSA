9b56bfe6fcbf404a2e4984c9884be151aecb940c
AN160404 <atianaim40@gmail.com>
2025-06-27 23:07:19 +0530

    Character Hashing

--- cpp/Hashing.cpp
+++ cpp/Hashing.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 int main(){
-    int nums[]={0,1,2,0,1,1,3,2};
-    int n=sizeof(nums);
-    int hash[n+1]={0};
+    string s="abdeasewasdczsd";
+    int n=s.length();
+    int hash[26]={0};
     for(int i=0;i<n;i++){
-        hash[nums[i]]++;
+        hash[s[i]-'a']++;
     }
-    for(int val:hash){
-        cout<<val<<endl;
-    }
+    cout<<"Frequency of d is: "<<hash['d'-'a'];
+    
     
 }
