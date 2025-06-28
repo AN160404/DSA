781e6ee2336bfcac797abc66e8539b19cc13a9ff
AN160404 <atianaim40@gmail.com>
2025-06-28 21:54:16 +0530

    3 Sum (Leetcode 15)

--- cpp/Hashing.cpp
+++ cpp/Hashing.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 int main()
 {
-    vector<int> nums = {1, 2, 3, 1, 5};
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
 
-    int slow = nums[0], fast = nums[0];
-    do
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n; i++)
     {
-        slow = nums[slow];       // +1
-        fast = nums[nums[fast]]; // +2
-    } while (slow != fast);
-    slow = nums[0];
-    while (slow != fast)
-    {
-        slow = nums[slow]; // +1
-        fast = nums[fast]; // +1
+        int j = i + 1, k = n - 1;
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        while (j < k)
+        {
+            if (nums[i] + nums[j] + nums[k] < 0)
+            {
+                j++;
+            }
+            else if (nums[i] + nums[j] + nums[k] > 0)
+            {
+                k--;
+            }
+            else
+            {
+                ans.push_back({nums[i], nums[j], nums[k]});
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+            }
+        }
+        
+        
     }
-    cout << slow;
+    for (int i = 0; i < ans.size(); i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cout<< ans[i][j]<<" ";
+            }
+            cout<<endl;
+            
+        }
 }
