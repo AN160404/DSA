1c231965987595cf9c9e35a42d867fbadf090f1a
AN160404 <atianaim40@gmail.com>
2025-06-30 01:28:25 +0530

    Contains Duplicate (Leetcode 217)

--- cpp/Hashing.cpp
+++ cpp/Hashing.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 int main()
 {
-    vector<int> nums = {1, 2, 3};
-    int k = 3;
-
-    int n = nums.size();
-    unordered_map<int, int> m;
-    m[0] = 1;
-    int count = 0, sum = 0;
-
-    for (int num : nums)
-    {
-        sum += num; // Contiguous sum for each number
-        if (m.find(sum - k) != m.end())  // If the remaining number found in the sums till now?
-        {                       
-            count += m[sum - k]; // Adding the frequency of remaining sum
+    vector<int> nums={1,2,3,1};
+    unordered_set<int> hash;
+        for(int val:nums){
+            if(hash.count(val)) return true;
+            hash.insert(val);
         }
-        m[sum]++; // If sum not present, adding the frequency of sum to map.
-    }
-    cout << count << endl;
+
+        return false;
 }
