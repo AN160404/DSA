05570b8ddeb0adc70cf0f906f8db36639f27b617
AN160404 <atianaim40@gmail.com>
2025-07-02 00:35:53 +0530

    Array Sorted Using Recursion

--- cpp/Recursion.cpp
+++ cpp/Recursion.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+
 using namespace std;
 
-int fib(int n){
-    if(n==1 || n==0) return n;
-    return fib(n-2)+fib((n-1));
+bool sorted(int arr[],int n){
+    if(n==0 || n==1) return true;
+    return (arr[n-1]>=arr[n-2]) && sorted(arr,n-1); // TC = O(n)
+    
 }
 int main(){
+    int arr[5]={1,2,3,41,5};
     int n=5;
-    int ans=fib(n);
-    cout<<ans<<endl;
+    int ans=sorted(arr,n);
+    if(ans==1) cout<<"Sorted"<<endl;
+    else cout<<"Not Sorted"<<endl;
     return 0;
 }
