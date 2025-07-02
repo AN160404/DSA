57e9e8ab6ca5bc7b765ec4df66111f90c298128e
AN160404 <atianaim40@gmail.com>
2025-07-02 19:20:33 +0530

    Subset 2 (Leetcode 90)

--- cpp/Recursion.cpp
+++ cpp/Recursion.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int subset(vector<int> arr,vector<int> ans, int i){  // O(n*2^n)
-    if(i==arr.size()){
-        for(int val:ans){
-            cout<<val<<" ";
-        }
-        cout<<endl;
+int subset(vector<int> &arr, vector<int> &ans, int i,vector<vector<int>> &allsub)
+{ // O(n*2^n)
+    if (i == arr.size())
+    {
+        allsub.push_back(ans);
         return 0;
     }
     // Including
     ans.push_back(arr[i]);
-    subset(arr,ans,i+1);
+    subset(arr, ans, i + 1,allsub);
 
     // Excluding
     ans.pop_back();
-    subset(arr,ans,i+1);
+
+    // Ensuring same element is not getting read twice
+
+    int idx=i+1;
+    while(idx<arr.size() && arr[idx]==arr[idx-1]){
+        idx++;
+    }
+    subset(arr,ans,idx,allsub);
 }
-int main(){
-    vector<int> arr={1,2,3};
+int main()
+{
+    vector<int> arr = {1, 2, 2};
     vector<int> ans;
-    int i=0;
-    subset(arr,ans,i);
+    vector<vector<int>> allsub;
+    int i = 0;
+    sort(arr.begin(),arr.end());
+
+    subset(arr, ans, i,allsub);
+
+    for (const auto& row : allsub) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
