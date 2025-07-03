dde1f5131bb9aa8e9ea19a9b42a2ddbdacbbf66f
AN160404 <atianaim40@gmail.com>
2025-07-04 00:22:26 +0530

    N-Queen using Recursion (Leetcode 51)

--- cpp/Recursion.cpp
+++ cpp/Recursion.cpp
@@ -1,39 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_set>
 using namespace std;
 
-int permutation(vector<int> &arr, vector<vector<int>> &ans, int idx)
-{ 
-    if (idx == arr.size())
-    {
-        ans.push_back({arr});
-        return 0;
+bool isSafe(vector<string> &board,int row,int col,int n){
+    // Check in row for different columns i.e Horizontally
+    for(int i=0;i<n;i++){
+        if(board[row][i]=='Q'){
+            return false;
+        }
     }
-    unordered_set<int> s;
-    for(int i=idx;i<arr.size();i++){
-        if(s.count(arr[i])) continue;
-        s.insert(arr[i]);
 
-        swap(arr[i],arr[idx]);
-        permutation(arr,ans,idx+1);
-        swap(arr[i],arr[idx]);
+    // Check in column for different rows i.e. Vertically
+    for(int i=0;i<row;i++){
+        if(board[i][col]=='Q'){
+            return false;
+        }
+    }
+
+    // Check for Left Diagonal
+    for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+        if(board[i][j]=='Q'){
+            return false;
+        }
+    }
+
+    // Check for Right Diagonal
+    for(int i=row,j=col;i>=0 && j<n;i--,j++){
+        if(board[i][j]=='Q'){
+            return false;
+        }
+    }
+    return true;
+
+}
+void nQueen(vector<string> &board,vector<vector<string>> &ans, int n,int row){
+    if(row==n){
+        ans.push_back(board);
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(isSafe(board,row, i,n)){
+            board[row][i]='Q';
+            nQueen(board,ans,n,row+1);
+            board[row][i]='.';
+        }
     }
 }
-int main()
-{
-    vector<int> arr = {1, 2, 2};
-    vector<vector<int>> ans;
-    int i = 0;
+int  main(){
+    int n=4;
+    vector<string> board(n,string(n,'.'));
+    vector<vector<string>> ans;
+    int row=0;
+    nQueen(board,ans,n,row);
 
-    permutation(arr, ans, i);
-
-    for (const auto& row : ans) {
-        for (int val : row) {
-            cout << val << " ";
+    for(const auto &val:ans){
+        for(const string &v: val){
+            cout<<v<<endl;
         }
-        cout << endl;
+        cout<<endl;
     }
     return 0;
 }
