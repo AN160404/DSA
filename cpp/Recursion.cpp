771c864f9dec681138fd677d42ab7607da96da8e
AN160404 <atianaim40@gmail.com>
2025-07-12 20:19:16 +0530

    Inversion Count

--- cpp/Recursion.cpp
+++ cpp/Recursion.cpp
@@ -1,38 +1,68 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int partition(vector<int>& nums, int lb, int ub) {
-    int i=lb,j=ub,pivot=nums[lb];
-    while(i<=j){
-        do{i++;} while(nums[i]<=pivot);
-        do{j--;} while(nums[j]> pivot);
-        if(i<j){
-            swap(nums[i],nums[j]);
+int merge(vector<int> &nums, int lb, int ub, int mid, vector<int> &temp)
+{
+    temp.clear();
+    int count = 0;
+    int i = lb, j = mid + 1;
+
+    while (i <= mid && j <= ub)
+    {
+        if (nums[i] <= nums[j])
+        {
+            temp.push_back(nums[i]);
+            i++;
         }
-        else{
-            swap(pivot,nums[j]);
-            return j;
+        else
+        {
+            temp.push_back(nums[j]);
+            count += (mid - i + 1); // Count inversions
+            j++;
         }
     }
+
+    // Append remaining elements
+    while (i <= mid)
+    {
+        temp.push_back(nums[i]);
+        i++;
+    }
+    while (j <= ub)
+    {
+        temp.push_back(nums[j]);
+        j++;
+    }
+
+    // Copy sorted elements back into nums
+    for (int k = 0; k < temp.size(); ++k)
+    {
+        nums[lb + k] = temp[k];
+    }
+
+    return count;
 }
 
-void quickSort(vector<int>& nums, int lb, int ub) {
-    if (lb >= ub) return; // Base case to stop recursion
-    int part=partition(nums,lb,ub);
-    quickSort(nums,lb,part);
-    quickSort(nums,part+1,ub);
+int mergeSort(vector<int> &nums, int lb, int ub, vector<int> &temp)
+{
+    if (lb >= ub)
+        return 0;
+
+    int mid = lb + (ub - lb) / 2;
+    int c1 = mergeSort(nums, lb, mid, temp);
+    int c2 = mergeSort(nums, mid + 1, ub, temp);
+    int c3 = merge(nums, lb, ub, mid, temp);
+    return c1 + c2 + c3;
 }
 
-int main() {
-    vector<int> nums = {4, 1, 5, 2, 3, 6, 7, 8};
+int main()
+{
+    vector<int> nums = {6, 3, 5, 2, 7};
     int n = nums.size();
     vector<int> temp;
-    quickSort(nums, 0, n - 1);
-
-    for (int i = 0; i < n; i++) {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
+    int count = mergeSort(nums, 0, n - 1, temp);
+    cout << "Inversion count: " << count << endl;
+    return 0;
 }
