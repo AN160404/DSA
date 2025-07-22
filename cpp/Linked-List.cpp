3615ba4d3f8a3b03479c63339203518d6a8d4d62
AN160404 <atianaim40@gmail.com>
2025-07-22 20:59:02 +0530

    CLL delete_at_position

--- cpp/Linked-List.cpp
+++ cpp/Linked-List.cpp
@@ -1,65 +1,84 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int val;
     Node *next;
 
-    Node(int data){
-        val=data;
-        next=NULL;
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
     }
 };
 
-class CLL{
-    Node* head;
-    Node* tail;
-    public:
-    CLL(){
-        head=NULL;
-        tail=NULL;
+class CLL
+{
+    Node *head;
+    Node *tail;
+
+public:
+    CLL()
+    {
+        head = NULL;
+        tail = NULL;
     }
 
-    void insert_at_tail(int val){
-        Node* newnode=new Node(val);
-        if(head==NULL){
-            head=tail=newnode;
-            tail->next=head;
+    void insert_at_tail(int val)
+    {
+        Node *newnode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newnode;
+            tail->next = head;
         }
-        tail->next=newnode;
-        tail=newnode;
-        tail->next=head;
+        tail->next = newnode;
+        tail = newnode;
+        tail->next = head;
     }
 
-    void insert_at_position(int pos, int val){
-        Node* newnode=new Node(val);
-        Node *ptr=head;
-        if(pos==0){
-            newnode->next=head;
-            tail->next=newnode;
-            head=newnode;
+    void delete_at_position(int pos)
+    {
+        Node *ptr = head;
+        if (pos == 0)
+        {
+            head = head->next;
+            tail->next = head;
+            delete ptr;
             return;
         }
-        for (int i = 0; i < pos-1; i++)
+        for (int i = 0; i < pos - 1; i++)
         {
-            ptr=ptr->next;    
+            ptr = ptr->next;
         }
-        newnode->next=ptr->next;
-        ptr->next=newnode;  
+
+        Node *temp = ptr->next;
+        ptr->next = temp->next;
+        if (temp == tail)
+        {
+            tail = ptr;
+            tail->next = head;
+        }
+
+        delete temp;
     }
 
-    void print(){
-        Node* ptr=head->next;
-        cout<<head->val<<" ";
-        while(ptr!=head){
-            cout<<ptr->val<<" ";
-            ptr=ptr->next;
+    void print()
+    {
+        Node *ptr = head->next;
+        cout << head->val << " ";
+        while (ptr != head)
+        {
+            cout << ptr->val << " ";
+            ptr = ptr->next;
         }
     }
 };
 
-int main(){
+int main()
+{
     CLL obj;
     obj.insert_at_tail(1);
     obj.insert_at_tail(2);
@@ -67,10 +86,9 @@ int main(){
 
     obj.print();
 
-    cout<<endl;
+    cout << endl;
 
-    obj.insert_at_position(1,0);
-    // obj.del_at_tail();
-    // obj.del_at_tail();
+    obj.delete_at_position(2);
+
     obj.print();
 }
