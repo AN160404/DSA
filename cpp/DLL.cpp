8f3a0720da717199e416c9f65a856d9ef39429fc
AN160404 <atianaim40@gmail.com>
2025-07-23 21:31:32 +0530

    Flatten List (Leetcode 430)

--- cpp/DLL.cpp
+++ cpp/DLL.cpp
@@ -1,75 +1,83 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
+// Definition for a Node.
+struct Node {
     int val;
-    Node* next;
     Node* prev;
+    Node* next;
+    Node* child;
 
-    Node(int val){
-        this->val=val;
-        next=prev=NULL;
+    Node(int _val) {
+        val = _val;
+        prev = NULL;
+        next = NULL;
+        child = NULL;
     }
 };
 
-class DLL{
-    Node *head;
-    Node *tail;
-    public:
-    DLL(){
-        head=tail=NULL;
+// Flatten function
+Node* flatten(Node* head) {
+    Node* curr = head;
+    while (curr != NULL) {
+        Node* next = NULL;
+        if (curr->child != NULL) {
+            next = curr->next;
+            curr->next = flatten(curr->child);
+            if (curr->next) curr->next->prev = curr;
+            curr->child = NULL;
+
+            while (curr->next != NULL) {
+                curr = curr->next;
+            }
+            if (next != NULL) {
+                curr->next = next;
+                next->prev = curr;
+            }
+        }
+        curr = curr->next;
     }
+    return head;
+}
 
-    void push_front(int num){
-        Node* newNode=new Node(num);
-        Node* ptr=head;
-        if(head==NULL && tail==NULL){
-            head=tail=newNode;
-            head->next=NULL;
-        }
-        else{
-            newNode->next=head;
-            head->prev=newNode;
-            head=newNode;
-        }
+// Helper: Print the doubly linked list
+void printList(Node* head) {
+    Node* curr = head;
+    while (curr) {
+        cout << curr->val << " ";
+        curr = curr->next;
     }
+    cout << endl;
+}
 
-    void reverseDLL(){
-        Node* temp=NULL;
-        Node* curr=head;
-        while(curr!=NULL){
-            // Swapping the next and prev
-            temp=curr->prev;
-            curr->prev=curr->next;
-            curr->next=temp;
-            // The next of curr would be prev due to switch
-            curr=curr->prev;
-        }
-        if(temp!=NULL){
-            // Last node in the list will be head
-            head=temp->prev;
-        }
-    }
+// Main for testing
+int main() {
+    // Level 1: 1 - 2 - 3 - 4 - 5
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->prev = head;
+    head->next->next = new Node(3);
+    head->next->next->prev = head->next;
+    head->next->next->next = new Node(4);
+    head->next->next->next->prev = head->next->next;
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->prev = head->next->next->next;
 
-    void print(){
-        Node* ptr=head;
-        while(ptr!=NULL){
-            cout<<ptr->val<<" ";
-            ptr=ptr->next;
-        }
-    }
-};
+    // Level 2: 7 - 8 - 9 attached to node 3
+    Node* child = new Node(7);
+    child->next = new Node(8);
+    child->next->prev = child;
+    child->next->next = new Node(9);
+    child->next->next->prev = child->next;
+    head->next->next->child = child;
 
-int main(){
-    DLL obj;
-    obj.push_front(1);
-    obj.push_front(2);
-    obj.push_front(3);
+    cout << "Original list:\n";
+    printList(head); // will only show 1-2-3-4-5
 
-    obj.print();
-    cout<<endl;
+    head = flatten(head);
 
-    obj.reverseDLL();
-    obj.print();
+    cout << "\nFlattened list:\n";
+    printList(head); // should show 1-2-3-7-8-9-4-5
+
+    return 0;
 }
