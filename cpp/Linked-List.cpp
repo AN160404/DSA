d3f9ba212e1b8318c683934ab6ddf9c8ad1f96e2
AN160404 <atianaim40@gmail.com>
2025-07-24 22:54:15 +0530

    Reverse K group (Leetcode 25)

--- cpp/Linked-List.cpp
+++ cpp/Linked-List.cpp
@@ -1,85 +1,99 @@
 #include <iostream>
+
 using namespace std;
 
-class Node
-{
+class Node {
 public:
-    int val;
-    Node *next;
+    int data;
+    Node* next;
 
-    Node(int data)
-    {
-        val = data;
+    Node(int val) {
+        data = val;
         next = NULL;
     }
 };
 
-class CLL
-{
-    Node *head;
-    Node *tail;
+class List {
+    Node* head;
+    Node* tail;
 
 public:
-    CLL()
-    {
-        head = NULL;
-        tail = NULL;
+    List() {
+        head = tail = NULL;
     }
 
-    void insert_at_tail(int val)
-    {
-        Node *newnode = new Node(val);
-        if (head == NULL)
-        {
-            head = tail = newnode;
-            tail->next = head;
-        }
-        tail->next = newnode;
-        tail = newnode;
-        tail->next = head;
-    }
-
-    void reverse(){
-        Node* prev=NULL;
-        Node* curr=head;
-        Node* next=NULL;
-        Node* start=head;
-        do{
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }while(curr!=start);
-
-        head->next=prev; // head is the old head and the new tail and after reversal prev is the new head
-        tail=head; // old head is the tail 
-        head=prev; // prev is the head
-    }
-
-    void print()
-    {
-        Node *ptr = head->next;
-        cout << head->val << " ";
-        while (ptr != head)
-        {
-            cout << ptr->val << " ";
+    // Printing LL
+    void print() {
+        Node* ptr = head;
+        while (ptr != NULL) {
+            cout << ptr->data << " ";
             ptr = ptr->next;
         }
+        cout << endl;
+    }
+
+    // Pushing the value in the Back of the Linked List
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    // Reverse nodes in k-group (return new head)
+    Node* reverseKGroup(Node* head, int k) {
+        Node*ptr=head;
+        Node* next=NULL;
+        int count=0;
+        while(count<k){
+            if(ptr==NULL) return head;
+            ptr=ptr->next;
+            count++;
+        }
+        Node* prevNode=reverseKGroup(ptr,k);
+
+        ptr=head;
+        count=0;
+        while(count<k){
+            next=ptr->next;
+            ptr->next=prevNode;
+            prevNode=ptr;
+            ptr=next;
+            count++;
+        }
+        return prevNode;
+    }
+
+    // Setter for head (to update after reverse)
+    void setHead(Node* newHead) {
+        head = newHead;
+    }
+
+    // Getter for head (to pass as argument)
+    Node* getHead() {
+        return head;
     }
 };
 
-int main()
-{
-    CLL obj;
-    obj.insert_at_tail(1);
-    obj.insert_at_tail(2);
-    obj.insert_at_tail(3);
+int main() {
+    List obj;
+    obj.push_back(1);
+    obj.push_back(2);
+    obj.push_back(3);
+    obj.push_back(4);
+    obj.push_back(5);
 
+    cout << "Original list: ";
     obj.print();
 
-    cout << endl;
+    Node* newHead = obj.reverseKGroup(obj.getHead(), 2);
+    obj.setHead(newHead);
 
-    obj.reverse();
-
+    cout << "Modified list: ";
     obj.print();
+
+    return 0;
 }
